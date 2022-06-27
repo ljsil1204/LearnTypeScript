1a5e46508c412a5493a7b5e75d7cb04f508dd501
이진실 <ljsil1204@naver.com>
2022-06-27 19:11:34 +0900

    타입스크립트 변수 선언
    - 에러 규칙 잠시 꺼두기

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx'],
       rules: {
+        '@typescript-eslint/no-unused-vars': 0, // 사용하는 곳 없는 변수 규칙 꺼줌. / 0 : 끔 ,  1 : 경고, 2 : 오류나타남
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
         'no-undef': 'off',
---
+++ learn.ts
@@ -0,0 +1,5 @@
+export default function () {}
+
+// 타입스크립트 적용 프로젝트 => 선언한 변수를 사용하는 곳이 없으면 에러 발생
+// 규칙 끄기 => .eslintrc.js 수정
+let message: string = 'Hello World';
