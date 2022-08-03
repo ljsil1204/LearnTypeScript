060f371230521080867e45ca42fcb7ff403f7b89
이진실 <ljsil1204@naver.com>
2022-08-03 18:29:18 +0900

    옵셔널 props , default props

--- Profile.tsx
+++ Profile.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
 interface Props {
   name: string;
+  isActive?: boolean;
+  image?: string;
 }
 
-function Profile({name}: Props) {
+function Profile({
+  name,
+  isActive,
+  image = 'https://blankspace-dev.tistory.com/200',
+}: Props) {
   return (
-    <View>
+    <View style={isActive && styles.activeStyle}>
+      <Image source={{uri: image}} />
       <Text>{name}</Text>
     </View>
   );
 }
+
+// Profile.defaultProps = {
+//   image: 'https://blankspace-dev.tistory.com/200',
+// };
+
+const styles = StyleSheet.create({
+  activeStyle: {
+    backgroundColor: 'yellow',
+  },
+});
 
 export default Profile;
