8ae8a5a890da38fcfc2580c87a40cf88ace6e43f
이진실 <ljsil1204@naver.com>
2022-08-03 17:58:31 +0900

    class Generic

--- learn.ts
+++ learn.ts
@@ -228,3 +228,23 @@ const PlaceItem: Item<Place> = {
 
 // Type Alias
 type Item2<T> = {id: number; data: T};
+
+// class Generic
+class Queue<T> {
+  list: T[] = [];
+  get length() {
+    return this.list.length;
+  }
+  enqueue(item: T) {
+    this.list.push(item);
+  }
+  dequeue() {
+    return this.list.shift();
+  }
+}
+
+const queue = new Queue<number>();
+queue.enqueue(0);
+queue.enqueue(1);
+const first = queue.dequeue();
+const second = queue.dequeue();
